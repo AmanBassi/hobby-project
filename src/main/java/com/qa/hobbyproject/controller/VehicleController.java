24c6f001f9c5a80501338b79c75c0f4604bd033e
Amandeep Bassi <asbassi@outlook.com>
2021-06-11 10:48:47 +0100

    Implemented add vehicle in the vehicle controller

--- src/main/java/com/qa/hobbyproject/controller/VehicleController.java
+++ src/main/java/com/qa/hobbyproject/controller/VehicleController.java
@@ -1,5 +1,23 @@
 package com.qa.hobbyproject.controller;
 
-public class VehicleController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.hobbyproject.domain.Vehicle;
+import com.qa.hobbyproject.service.VehicleService;
+
+@RestController
+public class VehicleController {
+	private VehicleService service;
+	
+	public VehicleController(VehicleService service) {
+		this.service = service;
+	}
+	
+	// Create
+	@PostMapping("/create")
+	public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
+		return this.service.addVehicle(vehicle);
+	}
 }
