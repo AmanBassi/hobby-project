e42a1e9aabf90ee19f937b9f0fedf6ddd61f78dc
Amandeep Bassi <asbassi@outlook.com>
2021-06-11 10:43:00 +0100

    Implemented create vehicle service

--- src/main/java/com/qa/hobbyproject/service/VehicleService.java
+++ src/main/java/com/qa/hobbyproject/service/VehicleService.java
@@ -1,5 +1,19 @@
 package com.qa.hobbyproject.service;
 
-public class VehicleService {
+import org.springframework.stereotype.Service;
 
+import com.qa.hobbyproject.domain.Vehicle;
+import com.qa.hobbyproject.repository.VehicleRepository;
+
+@Service
+public class VehicleService {
+	private VehicleRepository repository;
+	
+	public VehicleService(VehicleRepository repository) {
+		this.repository = repository;
+	}
+	
+	public Vehicle addVehicle(Vehicle vehicle) {
+		return this.repository.save(vehicle);
+	}
 }
