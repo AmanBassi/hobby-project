4ac5bc8d7f9761dc13fd08d7dd4a235ab5210be4
Amandeep Bassi <asbassi@outlook.com>
2021-06-16 22:45:40 +0100

    Added create vehicle task method to vehicle task service

--- src/main/java/com/qa/hobbyproject/service/VehicleTaskService.java
+++ src/main/java/com/qa/hobbyproject/service/VehicleTaskService.java
@@ -3,6 +3,8 @@ package com.qa.hobbyproject.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.qa.hobbyproject.domain.VehicleTask;
+import com.qa.hobbyproject.dto.VehicleTaskDTO;
 import com.qa.hobbyproject.repository.VehicleTaskRepository;
 
 @Service
@@ -13,6 +15,11 @@ public class VehicleTaskService {
 	public VehicleTaskService(VehicleTaskRepository repository, ModelMapper mapper) {
 		this.repository = repository;
 		this.mapper = mapper;
+	}
+	
+	public VehicleTaskDTO addVehicleTask(VehicleTask task) {
+		VehicleTask saved = this.repository.save(task);
+		return this.mapper.map(saved, VehicleTaskDTO.class);
 	}
 
 }
