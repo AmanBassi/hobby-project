a5349e2f2b5adf0491ea1f3e7daaa790786ee798
Amandeep Bassi <asbassi@outlook.com>
2021-06-17 12:34:32 +0100

    Created web form to update task details

--- src/main/resources/static/task.js
+++ src/main/resources/static/task.js
@@ -34,6 +34,24 @@
         return cell;
     }
 
+    const createUpdateButton = (task) => {
+        const cell = document.createElement("td");
+        const updateButton = document.createElement("button");
+        updateButton.innerText = "Update";
+        updateButton.className = "btn btn-primary";
+        updateButton.setAttribute("type", "button");
+        updateButton.setAttribute("data-bs-toggle", "modal");
+        updateButton.setAttribute("data-bs-target", "#updateVehicleTaskModal");
+        updateButton.addEventListener("click", function (event) {
+            document.getElementById("updateName").value = task.name;
+            document.getElementById("updateDueDate").value = task.dueDate;
+            document.getElementById("updateVehicleTaskButton").setAttribute("taskId", task.id);
+        });
+
+        cell.appendChild(updateButton)
+        return cell;
+    }
+
     const renderTask = task => {
         const tableBody = document.querySelector("tbody");
 
@@ -42,6 +60,7 @@
         tableRow.appendChild(createTableCell(task.id));
         tableRow.appendChild(createTableCell(task.name));
         tableRow.appendChild(createTableCell(task.dueDate));
+        tableRow.appendChild(createUpdateButton(task));
 
         tableBody.appendChild(tableRow);
     }
@@ -49,7 +68,6 @@
     const vehicleId = getVehicleId();
     getVehicle(vehicleId);
     getTasks(vehicleId);
-
 
     document.getElementById("createVehicleTaskForm").addEventListener("submit", function (event) {
         event.preventDefault();
@@ -76,6 +94,39 @@
 
                 document.getElementById("createVehicleTaskForm").classList.remove('was-validated');
                 document.getElementById("createVehicleTaskForm").reset();
+                getTasks(vehicleId);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        console.log(this);
+    });
+
+    document.getElementById("updateVehicleTaskForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+        if (!this.checkValidity()) {
+            event.stopPropagation();
+            console.log("form was not valid");
+            return;
+        }
+        this.classList.add('was-validated')
+        console.log("form was valid");
+
+        const taskId = document.getElementById("updateVehicleTaskButton").getAttribute("taskId");
+        const data = {
+            name: this.updateName.value,
+            dueDate: this.updateDueDate.value
+        }
+
+        axios.put(`/vehicletask/update/${taskId}`, data)
+            .then(function (response) {
+                console.log(response);
+                var myModalElement = document.getElementById('updateVehicleTaskModal');
+                var modal = bootstrap.Modal.getInstance(myModalElement);
+                modal.hide();
+
+                document.getElementById("updateVehicleTaskForm").classList.remove('was-validated');
+                document.getElementById("updateVehicleTaskForm").reset();
                 getTasks(vehicleId);
             })
             .catch(function (error) {
